d32c357171c79557001d01eefa71964958e4634e
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-04 01:20:37 -0500

    Enhance Feed component with archive button and icon for better user interaction

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CallList from "../Components/CallList.jsx";
+import { IoArchiveOutline } from "react-icons/io5";
 
 const Feed = () => {
   return (
     <section>
-      <div>This is the user feed</div>
-      <button>Archive all calls</button>
+      <div className="archive-all-button">
+        <IoArchiveOutline size={24} />
+        <h2>Archive all calls</h2>
+      </div>
       <CallList />
     </section>
   );
