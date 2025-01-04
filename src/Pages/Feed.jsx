acdb082b805f19a2d923402c67b4b0f13aa54e51
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-04 14:10:05 -0500

    Remove archive button from Feed component and update section styling

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import CallList from "../Components/CallList.jsx";
-import { IoArchiveOutline } from "react-icons/io5";
 
 const Feed = () => {
   return (
-    <section>
-      <div className="archive-all-button">
-        <IoArchiveOutline size={24} />
-        <h2>Archive all calls</h2>
-      </div>
+    <section className="inbox-section">
       <CallList />
     </section>
   );
