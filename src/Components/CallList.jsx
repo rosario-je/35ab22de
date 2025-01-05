a42502a065fd609274066d46ab393bda6c2fc965
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-04 21:33:07 -0500

    Add loading state and improve call archiving functionality in CallList component

--- src/Components/CallList.jsx
+++ src/Components/CallList.jsx
@@ -8,8 +8,8 @@ import {
   BsFillTelephoneOutboundFill,
 } from "react-icons/bs";
 import { IoInformationCircleSharp } from "react-icons/io5";
-import { IoArchiveOutline } from "react-icons/io5";
 import CallDetailsModal from "./CallDetailsModal.jsx";
+import LoadingScreen from "./LoadingScreen.jsx";
 
 const CallList = () => {
   const {
@@ -20,19 +20,20 @@ const CallList = () => {
     setArchivedCalls,
     handleArchiveAll,
     handleCallReset,
+    loading,
   } = useContext(AppContext);
 
   const [selectedCall, setSelectedCall] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Sort and filter calls based on selected page
-  const sortedCallList = callList.sort((a, b) => {
+  const sortedCallList = [...callList].sort((a, b) => {
     const dateA = new Date(a.created_at);
     const dateB = new Date(b.created_at);
     return dateB - dateA;
   });
 
-  const filteredCallList = callList.filter((call) => {
+  const filteredCallList = sortedCallList.filter((call) => {
     if (selectedPage === "Inbox") {
       return call.direction === "inbound";
     }
@@ -50,86 +51,113 @@ const CallList = () => {
     setIsModalOpen(false);
   };
 
+  const message =
+    selectedPage === "Archived" ? "Unarchiving calls" : "Archiving calls";
+
   return (
     <>
-      {selectedPage === "Archived" && archivedCalls.length > 0 ? (
-        <button className="archive-all-button pb-4" onClick={handleCallReset}>
-          <h2>Unarchive all calls</h2>
-        </button>
+      {loading ? (
+        <LoadingScreen message={message} />
       ) : (
-        selectedPage !== "Archived" &&
-        callList.length > 0 && (
-          <button
-            className="archive-all-button pb-4"
-            onClick={handleArchiveAll}
-          >
-            <h2>Archive all calls</h2>
-          </button>
-        )
-      )}
-
-      <div className="call-list overflow-y-auto scrollbar-hidden max-h-[400px]">
-        {selectedPage === "Archived" ? (
-          archivedCalls.length > 0 ? (
-            archivedCalls.map((call) => (
-              <div key={call.id} className="call-container">
-                <div className="call-icon">
-                  {call.direction === "inbound" ? (
-                    <BsFillTelephoneInboundFill size={20} />
-                  ) : (
-                    <BsFillTelephoneOutboundFill size={20} />
-                  )}
-                </div>
-                <div className="call-details">
-                  <h2>+{call.from}</h2>
-                  <div className="call-action">
-                    <p className="call-date">{formatDate(call.created_at)}</p>
-                    <IoInformationCircleSharp
-                      size={20}
-                      onClick={() => handleOpenModal(call)}
-                    />
-                  </div>
-                </div>
-              </div>
-            ))
+        <>
+          {/* Archive/Unarchive All Button */}
+          {selectedPage === "Archived" && archivedCalls.length > 0 ? (
+            <button
+              className="archive-all-button pb-4"
+              onClick={handleCallReset}
+            >
+              <h2>Unarchive all calls</h2>
+            </button>
           ) : (
-            <p>No archived calls to display</p>
-          )
-        ) : filteredCallList.length > 0 ? (
-          filteredCallList
-            .filter((call) => call.is_archived !== "true")
-            .map((call) => (
-              <div key={call.id} className="call-container ">
-                <div className="call-icon">
-                  {call.direction === "inbound" ? (
-                    <BsFillTelephoneInboundFill size={20} />
-                  ) : (
-                    <BsFillTelephoneOutboundFill size={20} />
-                  )}
-                </div>
-                <div className="call-details">
-                  <h2>+{call.from}</h2>
-                  <div className="call-action">
-                    <p className="call-date">{formatDate(call.created_at)}</p>
-                    <IoInformationCircleSharp
-                      size={20}
-                      onClick={() => handleOpenModal(call)}
-                    />
-                  </div>
-                </div>
-              </div>
-            ))
-        ) : (
-          <p>No calls to display</p>
-        )}
-      </div>
+            selectedPage !== "Archived" &&
+            callList.length > 0 && (
+              <button
+                className="archive-all-button pb-4"
+                onClick={handleArchiveAll}
+              >
+                <h2 className="text-zinc-700 hover:text-red-800">
+                  Archive all calls
+                </h2>
+              </button>
+            )
+          )}
 
-      {selectedCall && (
-        <CallDetailsModal
-          {...selectedCall}
-          isOpen={isModalOpen}
-          onClose={handleCloseModal}
-        />
+          {/* Call List */}
+          <div className="call-list overflow-y-auto scrollbar-hidden max-h-[400px]">
+            {selectedPage === "Archived" ? (
+              archivedCalls.length > 0 ? (
+                archivedCalls.map((call) => (
+                  <div key={call.id} className="call-container">
+                    <div className="call-icon">
+                      {call.direction === "inbound" ? (
+                        <BsFillTelephoneInboundFill size={20} />
+                      ) : (
+                        <BsFillTelephoneOutboundFill size={20} />
+                      )}
+                    </div>
+                    <div className="call-details">
+                      <h2>+{call.from}</h2>
+                      <div className="call-action">
+                        <p className="call-date">
+                          {formatDate(call.created_at)}
+                        </p>
+                        <IoInformationCircleSharp
+                          size={20}
+                          onClick={() => handleOpenModal(call)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className="flex w-full h-screen justify-center items-center">
+                  <p>No archived calls to display</p>
+                </div>
+              )
+            ) : filteredCallList.length > 0 ? (
+              filteredCallList
+                .filter((call) => !call.is_archived)
+                .map((call) => (
+                  <div key={call.id} className="call-container">
+                    <div className="call-icon">
+                      {call.direction === "inbound" ? (
+                        <BsFillTelephoneInboundFill size={20} />
+                      ) : (
+                        <BsFillTelephoneOutboundFill size={20} />
+                      )}
+                    </div>
+                    <div className="call-details">
+                      <h2>+{call.from}</h2>
+                      <div className="call-action">
+                        <p className="call-date">
+                          {formatDate(call.created_at)}
+                        </p>
+                        <IoInformationCircleSharp
+                          size={20}
+                          onClick={() => handleOpenModal(call)}
+                          className="text-zinc-500 transition-all hover:text-zinc-700 cursor-pointer"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))
+            ) : (
+              <div className="flex w-full h-[500px] justify-center items-center">
+                <p>No calls to display</p>
+              </div>
+            )}
+          </div>
+
+          {/* Call Details Modal */}
+          {selectedCall && (
+            <CallDetailsModal
+              {...selectedCall}
+              isOpen={isModalOpen}
+              onClose={handleCloseModal}
+              selectedPage={selectedPage}
+            />
+          )}
+        </>
       )}
     </>
   );
