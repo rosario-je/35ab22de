f247bfdbef629cdec8f52b1193673b46d91cad5c
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-05 00:52:48 -0500

    Enhance BottomNav component to display call count notification and improve icon sizes for better usability

--- src/Components/BottomNav.jsx
+++ src/Components/BottomNav.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+
+//Context
+import { AppContext } from "../Context/AppContext.jsx";
+
+
+//Icons
 import { IoCallSharp, IoSettingsSharp } from "react-icons/io5";
 import { FaUserLarge, FaUsers } from "react-icons/fa6";
+import { PiNumpad } from "react-icons/pi";
+
+
+
 
 const BottomNav = () => {
+  const { callList } = useContext(AppContext);
+
   return (
     <nav className="bottom-nav">
-      <ul className="bottom-nav-list">
-        <li className="text-green-600" onClick={() => navigate("/")}>
-          <IoCallSharp />
+      <ul className="bottom-nav-list flex justify-evenly">
+        <li className="relative text-green-600" onClick={() => navigate("/")}>
+          <IoCallSharp size={20} />
+          {callList.length > 0 && (
+            <span className="absolute -top-2 left-2 w-4 h-4 flex items-center justify-center text-xs text-white bg-red-500 rounded-full">
+              {callList.length}
+            </span>
+          )}
         </li>
         <li>
-          <FaUserLarge />
+          <FaUserLarge size={20} />
         </li>
         <li>
-          <FaUsers />
+          <PiNumpad size={30} />
         </li>
         <li>
-          <IoSettingsSharp />
+          <FaUsers size={20} />
+        </li>
+        <li>
+          <IoSettingsSharp size={20} />
         </li>
       </ul>
     </nav>
