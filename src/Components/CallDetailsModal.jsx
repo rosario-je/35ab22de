475881dcbf094da52fd3cf3dca50edcbfcfd99ea
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-04 21:31:55 -0500

    Enhance CallDetailsModal with loading state and dynamic archive functionality

--- src/Components/CallDetailsModal.jsx
+++ src/Components/CallDetailsModal.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-
-import { FaArchive } from "react-icons/fa";
-
+import React, { useState } from "react";
 import { updateCall } from "../API";
 
 const CallDetailsModal = ({
@@ -13,62 +10,116 @@ const CallDetailsModal = ({
   via,
   duration,
   is_archived,
+  call_type,
   isOpen,
   onClose,
-  refreshCallList, // Added refreshCallList as a prop
+  selectedPage,
 }) => {
+  const [loading, setLoading] = useState(false);
+
+  const formatDuration = (duration) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+    return `${minutes}m ${seconds}s`;
+  };
+
   const handleCallUpdate = async () => {
+    setLoading(true);
+
     try {
+      // Determine the new archive status
+      const newArchiveStatus =
+        selectedPage === "Archived" || is_archived ? false : true;
+
+
       const updatedCall = await updateCall(id, {
-        is_archived: true,
+        is_archived: newArchiveStatus,
       });
       console.log(`Updated call ${id}:`, updatedCall);
 
-      // Refresh the call list after archiving
-      refreshCallList();
-
-      // Close the modal after archiving
       onClose();
     } catch (error) {
       console.error(`Failed to update call ${id}:`, error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const formattedDate = new Date(created_at).toLocaleString();
 
   return (
     <div
       id="modal-container"
       tabIndex="-1"
-      className={`fixed inset-0 flex items-center justify-center transition-opacity duration-300 ${
+      className={`fixed inset-0 flex items-center justify-center duration-300 ${
         isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
-      style={{ transition: "opacity 0.3s ease-in-out" }}
     >
       <div
-        className={`modal-box w-[350px] mr-4 transition-transform duration-300 ${
+        className={`modal-box w-[350px] max-w-sm mx-4 bg-white rounded-lg shadow-lg transform transition-all duration-300 border ${
           isOpen ? "scale-100" : "scale-95"
         }`}
       >
-        <div className="flex justify-between items-center w-full">
-          {direction === "inbound" ? (
-            <h3 className="text-zinc-100">
-              <span>Inbound Caller:</span> {from}
+        <div className="flex justify-between items-center py-4 px-6 border-b border-zinc-200">
+          <div>
+            <h3 className="text-[15px] font-semibold text-zinc-800">
+              {direction === "inbound" ? (
+                <>
+                  <span className=" text-zinc-800">Inbound Caller:</span>{" "}
+                  <span className="italic">{from}</span>
+                </>
+              ) : (
+                <>
+                  <span className="text-sm text-zinc-800">You called:</span>{" "}
+                  {from}
+                </>
+              )}
             </h3>
-          ) : (
-            <h3 className="text-zinc-100">
-              <span>You called:</span> {from}
-            </h3>
-          )}
-          <button onClick={handleCallUpdate}>
-            <FaArchive className="text-white" />
-          </button>
+            <p
+              className={`text-sm ${
+                call_type === "missed" ? "text-red-600" : "text-zinc-800"
+              }`}
+            >
+              {call_type}
+            </p>
+          </div>
         </div>
-        <p className="py-4">Call duration: {duration} seconds</p>
-        <div className="modal-action">
-          <form method="dialog">
-            <button type="button" className="btn btn-sm" onClick={onClose}>
-              Close
-            </button>
-          </form>
+
+        <div className="py-4 px-6 space-y-3">
+          <p className="text-sm text-zinc-600">Call Date: {formattedDate}</p>
+          <p className="text-sm text-zinc-600">
+            Call Duration: {formatDuration(duration)}
+          </p>
+          <p className="text-sm text-zinc-600">Via: {via}</p>
+        </div>
+        <div className="flex justify-end p-4 border-t border-zinc-200">
+          <button
+            id="close-button"
+            type="button"
+            className="px-4 py-2 text-sm font-semibold text-blue-600 bg-zinc-200 rounded-lg hover:bg-zinc-300 focus:outline-none"
+            onClick={onClose}
+          >
+            Close
+          </button>
+          <button
+            id="archive-button"
+            type="button"
+            className={`ml-2 px-4 py-2 text-sm font-semibold rounded-lg focus:outline-none ${
+              selectedPage === "Archived" || is_archived
+                ? "text-green-600 bg-zinc-200 hover:bg-zinc-300"
+                : "text-red-600 bg-zinc-200 hover:bg-zinc-300"
+            }`}
+            onClick={handleCallUpdate}
+            disabled={loading}
+          >
+            {loading
+              ? selectedPage === "Archived" || is_archived
+                ? "Unarchiving..."
+                : "Archiving..."
+              : selectedPage === "Archived" || is_archived
+              ? "Unarchive"
+              : "Archive"}
+          </button>
         </div>
       </div>
     </div>
