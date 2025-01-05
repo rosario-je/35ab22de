0eddc457b3b04af063fc7f390e0c12b72a7557f9
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-05 01:56:08 -0500

    Refactor CallDetailsModal to improve date and time formatting, enhance layout, and adjust styling for better readability

--- src/Components/CallDetailsModal.jsx
+++ src/Components/CallDetailsModal.jsx
@@ -49,7 +49,11 @@ const CallDetailsModal = ({
     }
   };
 
-  const formattedDate = new Date(created_at).toLocaleString();
+  const formattedDate = new Date(created_at).toLocaleDateString();
+  const formattedTime = new Date(created_at).toLocaleTimeString();
+  const dayOfWeek = new Date(created_at).toLocaleDateString("en-US", {
+    weekday: "long",
+  });
 
   return (
     <div
@@ -64,7 +68,7 @@ const CallDetailsModal = ({
           isOpen ? "scale-100" : "scale-95"
         }`}
       >
-        <div className="flex justify-between items-center py-4 px-6 border-b border-zinc-200">
+        <div className="flex justify-between items-center py-2  border-b border-zinc-200">
           <div>
             <h3 className="text-[15px] font-semibold text-zinc-800">
               {direction === "inbound" ? (
@@ -93,18 +97,26 @@ const CallDetailsModal = ({
           </div>
         </div>
 
-        <div className="py-4 px-6 space-y-3">
-          <p className="text-sm text-zinc-600">Call Date: {formattedDate}</p>
-          <p className="text-sm text-zinc-600">
-            Call Duration: {formatDuration(duration)}
-          </p>
-          <p className="text-sm text-zinc-600">Via: {via}</p>
+        <div className="py-2 space-y-3">
+          <div className="call-info">
+            <p className="text-[12px] text-zinc-600 pb-2">
+              {dayOfWeek} {formattedDate}
+            </p>
+            <div className="call-time-info flex flex-col gap-y-1">
+              <p className="text-[10px] text-zinc-600">{formattedTime}</p>
+              <p className="text-[10px] text-zinc-600">
+                {formatDuration(duration)}
+              </p>
+            </div>
+          </div>
+
+          <p className="text-[10px] text-zinc-600">Via: {via}</p>
         </div>
         <div className="flex justify-end pt-4 border-t border-zinc-200">
           <button
             id="close-button"
             type="button"
-            className="px-4 py-2 text-sm font-semibold text-blue-600 bg-zinc-200 rounded-lg hover:bg-zinc-300 transition-all duration-200 focus:outline-none"
+            className="button button-xs px-4 py-1 text-sm font-semibold text-blue-600 bg-zinc-200 rounded-lg hover:bg-zinc-300 transition-all duration-200 focus:outline-none"
             onClick={onClose}
           >
             Close
